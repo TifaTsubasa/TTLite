a0be4b14102c64c2011613a289c51f9176b962af
TifaTsubasa <15922402179@163.com>
2016-02-16 17:02:19 +0800

    Initial Commit

---
+++ TTLite/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TTLite
+//
+//  Created by TifaTsubasa on 16/2/16.
+//  Copyright © 2016年 tifatsubasa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TTLite/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TTLite
+//
+//  Created by TifaTsubasa on 16/2/16.
+//  Copyright © 2016年 tifatsubasa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
